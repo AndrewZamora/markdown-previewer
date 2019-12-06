d78f8af7c5abc873c14854cd328eae9ff053f87a
AndrewZamora <andrewmichaelzamora@gmail.com>
2019-12-07 00:34:21 +0900

    added marked library to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+
+import React, { Component } from 'react'
+import marked from 'marked';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <textarea id="editor"></textarea>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
+  };
+  convertToMarkdown = text => {
+    return (marked(text));
+  }
+  handleInput = event => {
+    const input = event.target.value;
+    // const markdown =;
+    this.setState({
+      input: input
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <textarea id="editor"  onChange={(e)=>this.handleInput(e)}></textarea>
+        <div id="preview" className="preview">
+          {this.convertToMarkdown(this.state.input)}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
